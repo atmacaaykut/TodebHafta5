2321ca8f9ff50bb22c2b7fdb37f8d736067506da
Murat Kurtboğan <murat.kurtbogan@odeontechnology.com>
2022-08-06 12:12:18 +0300

    .

---
+++ API/Configuration/Filters/Logs/LogFilterAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Console = System.Console;
+
+
+namespace API.Configuration.Filters.Log
+{
+    public class LogFilterAttribute: Attribute, IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var msLogger = context.HttpContext.RequestServices.GetService<MsSqlLogger>();
+            var data = context.ActionArguments.Values;
+            var logStr = System.Text.Json.JsonSerializer.Serialize(data);
+
+            msLogger.LoggerManager.Information(logStr);
+
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+           Console.WriteLine("İşlem tamamlandı");
+        }
+    }
+}
---
+++ API/Configuration/Filters/Logs/MsSqlLogger.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
+
+namespace API.Configuration.Filters.Log
+{
+
+    public class MsSqlLogger
+    {
+        public ILogger LoggerManager;
+        public MsSqlLogger(IConfiguration configuration)
+        {
+            var sinkOpt = new MSSqlServerSinkOptions()
+            {
+                TableName = "Logs",
+                AutoCreateSqlTable = true
+            };
+
+            var columnOpts = new ColumnOptions();
+            columnOpts.Store.Remove(StandardColumn.Message);
+            columnOpts.Store.Remove(StandardColumn.Properties);
+
+            var seriLogConf = new LoggerConfiguration().WriteTo
+                .MSSqlServer(
+                    connectionString: configuration.GetConnectionString("MsComm"),
+                    sinkOptions: sinkOpt,
+                    columnOptions: columnOpts); //builder deseni
+
+
+            LoggerManager = seriLogConf.CreateLogger();
+        }
+    }
+}
